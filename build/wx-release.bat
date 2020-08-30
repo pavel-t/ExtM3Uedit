3cd8ac2fdf8b30352b544d2ab6900fa8f470bf97
Pavel Tyunin <pavel51tunin@gmail.com>
2020-08-30 18:39:47 +0300

    Update build scripts and config
    Enable C++20 and parallel build

--- build/wx-release.bat
+++ build/wx-release.bat
@@ -1,4 +1,5 @@
 cd ..\dep\wxWidgets\build\msw
-mingw32-make -s -f makefile.gcc CFG="-release" BUILD=release DEBUG_FLAG=1 CPPFLAGS="-flto" CXXFLAGS="-std=c++17"
+mingw32-make -s -f makefile.gcc CFG="-release" BUILD=release DEBUG_FLAG=1 CPPFLAGS="-flto" CXXFLAGS="-std=c++20" setup_h
+mingw32-make -s -f makefile.gcc CFG="-release" BUILD=release DEBUG_FLAG=1 CPPFLAGS="-flto" CXXFLAGS="-std=c++20" -j4
 rmdir gcc_mswu-release /s /q
 pause
